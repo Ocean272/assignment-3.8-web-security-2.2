4766c53060f3ba82fd1fea92d9ffd0cbb66fd7ae
Amy Lim H L <66409683+Ocean272@users.noreply.github.com>
2022-05-01 12:56:10 +0800

    updated cannot create table

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
     HOST: "localhost",
-    USER: "root",
-    PASSWORD: "123456",
-    DB: "testdb",
+    USER: "postgres",
+    PASSWORD: "ocean272",
+    DB: "makan_apa",
     dialect: "postgres",
     pool: {
         max: 5,
--- app/models/index.js
+++ app/models/index.js
@@ -3,12 +3,12 @@ const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(
     config.DB,
-    config.postgres,
-    config.ocean272,
+    config.USER,
+    config.PASSWORD,
     {
         host: config.HOST,
         dialect: config.dialect,
-        operatorsAliases: false,
+        operatorsAliases: 0,
         pool: {
             max: config.pool.max,
             min: config.pool.min,
